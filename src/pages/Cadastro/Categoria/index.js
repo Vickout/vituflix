766072b5321a2636c108af4c6515e466486c98c7
Victor Lourenco <vlourenco.m@gmail.com>
2020-07-31 14:02:41 -0300

    Added backend to app

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,45 +1,56 @@
-import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 export default function CadastroCategoria() {
   const initialValues = {
-    name: "",
-    description: "",
-    color: "#000000"
+    name: '',
+    description: '',
+    color: '#000000',
   };
 
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(initialValues);
-  
 
   function HandleSubmit(e) {
     e.preventDefault();
     setCategorias([...categorias, values]);
 
     setValues(initialValues);
-  };
+  }
 
   function HandleValue(key, value) {
     setValues({
       ...values,
       [key]: value, // nome: valor
-    })
-  };
+    });
+  }
 
   function HandleChange(event) {
-    //const { getAttribute, value } = event.target;
+    // const { getAttribute, value } = event.target;
     HandleValue(event.target.getAttribute('name'), event.target.value);
-  };
+  }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL).then(async (res) => {
+      const resposta = await res.json();
+      setCategorias([...resposta]);
+    });
+  }, []);
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.name}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.name}
+      </h1>
 
       <form onSubmit={HandleSubmit}>
 
-        <FormField 
+        <FormField
           label="Nome da Categoria: "
           type="text"
           name="name"
@@ -47,7 +58,7 @@ export default function CadastroCategoria() {
           onChange={HandleChange}
         />
 
-        <FormField 
+        <FormField
           label="Descrição: "
           type="textarea"
           name="description"
@@ -55,23 +66,27 @@ export default function CadastroCategoria() {
           onChange={HandleChange}
         />
 
-        <FormField 
+        <FormField
           label="Cor: "
           type="color"
           name="color"
           value={values.color}
           onChange={HandleChange}
         />
-        
+
+        {categorias.length === 0 && (
+          <div>
+            Loading...
+          </div>
+        )}
+
         <ul>
-          {categorias.map((categoria, index) => {
-            return (
-              <li key={index}>{categoria.name}</li>
-            );
-          })}
+          {categorias.map((categoria, index) => (
+            <li key={index}>{categoria.name}</li>
+          ))}
         </ul>
 
-        <button>Cadastrar</button>
+        <Button>Cadastrar</Button>
       </form>
 
       <Link to="/">
